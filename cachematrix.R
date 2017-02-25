c8313aa9319c08d70de82b21b361e6665f1c8746
flarossler <flavia_rossler@yahoo.com.au>
2017-02-25 13:30:55 +1100

    CacheMatrix Assignment - changes in Markdown

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,11 @@
-#This function makeCacheMatrix creates a matrix which is a list containing a function to
-# 1. Set the value of the matrix
-# 2. Get the value of the matrix
-# 3. Set the inverse of the matrix
-# 4. Get the inverse of the matrix
+This function makeCacheMatrix creates a matrix which is a list containing a function to
+1. Set the value of the matrix
+2. Get the value of the matrix
+3. Set the inverse of the matrix
+4. Get the inverse of the matrix
 
+<!-- -->
+    
 makeCacheMatrix <- function(x = matrix()) {
     invMtx <- NULL
     set <- function(y) {
@@ -18,10 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## The following function calculates the inverse (solve) of the matrix created with makeCacheMatrix function. 
-## It first checks to see if the inverse of the matrix has already been calculated. If so, it gets the information from the cache and skips the computation. 
-## Otherwise, it calculates the inverse of the matrix and sets the value of the inverse in the cache via the setsolve function.
 
+The following function calculates the inverse (solve) of the matrix created with makeCacheMatrix function. 
+It first checks to see if the inverse of the matrix has already been calculated. If so, it gets the information from the cache and skips the computation. 
+]Otherwise, it calculates the inverse of the matrix and sets the value of the inverse in the cache via the setsolve function.
+
+<!-- -->
+    
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     m <- x$getsolve()
